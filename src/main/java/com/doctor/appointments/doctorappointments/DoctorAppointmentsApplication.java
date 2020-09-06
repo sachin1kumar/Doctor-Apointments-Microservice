692fd8ea95b54d33e1009c7de217661d492344b2
Sachin <sachinkmr375@gmail.com>
2020-09-06 22:10:58 +0530

    registered as eureka client

--- src/main/java/com/doctor/appointments/doctorappointments/DoctorAppointmentsApplication.java
+++ src/main/java/com/doctor/appointments/doctorappointments/DoctorAppointmentsApplication.java
@@ -2,9 +2,31 @@ package com.doctor.appointments.doctorappointments;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class DoctorAppointmentsApplication {
+	@Bean
+	@LoadBalanced
+	public RestTemplate getRestTemplate(){
+		HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+		requestFactory.setConnectTimeout(1000);
+		requestFactory.setReadTimeout(1000);
+		RestTemplate restTemplate = new RestTemplate();
+		restTemplate.setRequestFactory(requestFactory);
+		return restTemplate;
+	}
+
+	@Bean
+	public WebClient.Builder getWebClientBuilder(){
+		return WebClient.builder();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DoctorAppointmentsApplication.class, args);
--- src/main/java/com/doctor/appointments/doctorappointments/controllers/DoctorAppointmentsController.java
+++ src/main/java/com/doctor/appointments/doctorappointments/controllers/DoctorAppointmentsController.java
@@ -2,10 +2,14 @@ package com.doctor.appointments.doctorappointments.controllers;
 
 import com.doctor.appointments.doctorappointments.model.DoctorAppointments;
 import com.doctor.appointments.doctorappointments.repositories.DoctorAppointmentsRepository;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.math.BigInteger;
 
@@ -16,10 +20,15 @@ public class DoctorAppointmentsController {
     @Autowired
     private DoctorAppointmentsRepository doctorAppointmentsRepository;
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     @RequestMapping(value = "/", method = RequestMethod.POST)
     public ResponseEntity<String> setAppointmentFlag(@RequestBody DoctorAppointments doctorAppointments) {
         if (doctorAppointmentsRepository.existsById(doctorAppointments.getId())) {
-            //call doctor appointments microservice..
+            //call doctor appointments update microservice..
+            final HttpEntity<ObjectNode> requestEntity = new HttpEntity(doctorAppointments);
+            restTemplate.exchange("http://doctor-update-appointments-service/doctor-update-appointments/", HttpMethod.PATCH, requestEntity, DoctorAppointments.class);
         } else {
             doctorAppointmentsRepository.save(doctorAppointments);
         }
